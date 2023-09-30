f69651f007635ac116c9dd9b0c653d71de3b89b5
Supriyo Dey <supriyodeyiosdev@gmail.com>
2023-09-30 15:24:14 +0530

    supriyo/initial

--- Elements Collection/Custom segment/CustomSegmentedControl.swift
+++ Elements Collection/Custom segment/CustomSegmentedControl.swift
@@ -5,4 +5,105 @@
 //  Created by Supriyo Dey on 30/09/23.
 //
 
-import Foundation
+import UIKit
+
+class CustomSegmentedControl: UIControl {
+    var buttons = [UIButton]()
+    var selector: UIView!
+    var selectedSegmentIndex = 0
+    
+    var bgColor: UIColor = UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.00)
+    
+    var buttonTitles: [String] = [] {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButtons()
+    }
+    
+    private func setupButtons() {
+        layer.cornerRadius = 25
+        backgroundColor = bgColor
+        // Remove existing buttons and selector
+        buttons.forEach { $0.removeFromSuperview() }
+        buttons.removeAll()
+        selector?.removeFromSuperview()
+        
+        
+        // Add selector view
+        let selectorWidth = frame.width / CGFloat(buttonTitles.count)
+        let selectorHeight = frame.height
+        
+        
+        selector = UIView(frame: CGRect(x: 5, y: 5, width: selectorWidth-10, height: selectorHeight-10))
+        selector.layer.cornerRadius = 20
+        selector.backgroundColor = UIColor(red: 0.16, green: 0.56, blue: 0.85, alpha: 1.00)
+        addSubview(selector)
+        
+        for (index, title) in buttonTitles.enumerated() {
+            let button = UIButton(type: .custom)
+            button.setTitle(title, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+            button.setTitleColor(selectedSegmentIndex==index ? .white : UIColor(red: 0.47, green: 0.47, blue: 0.47, alpha: 1.00), for: .normal)
+            button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+            buttons.append(button)
+            addSubview(button)
+        }
+
+        
+        setNeedsLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        var buttonX: CGFloat = 0
+        let buttonY: CGFloat = 0
+        let buttonWidth = frame.width / CGFloat(buttons.count)
+        let buttonHeight = frame.height
+        
+        for button in buttons {
+            button.frame = CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight)
+            buttonX += buttonWidth
+        }
+        
+        //up[date selector width
+        let selectorWidth = frame.width / CGFloat(buttonTitles.count)
+        let selectorHeight = frame.height
+        selector.frame = CGRect(x: 5, y: 5, width: selectorWidth-10, height: selectorHeight-10)
+        
+        // Set initial selector position based on selected segment index
+        let selectorX = buttonWidth * CGFloat(selectedSegmentIndex)
+        selector.frame.origin.x = selectorX+5
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        for (index, button) in buttons.enumerated() {
+            if button == sender {
+                selectedSegmentIndex = index
+            }
+        }
+        
+        // Animate selector movement
+        UIView.animate(withDuration: 0.3) {
+            let selectorX = self.frame.width / CGFloat(self.buttons.count) * CGFloat(self.selectedSegmentIndex)
+            self.selector.frame.origin.x = selectorX+5
+            for btn in self.buttons {
+                btn.setTitleColor(UIColor(red: 0.47, green: 0.47, blue: 0.47, alpha: 1.00), for: .normal)
+            }
+            self.buttons[self.selectedSegmentIndex].setTitleColor(.white, for: .normal)
+        }
+        
+        // Notify the target of the value change
+        sendActions(for: .valueChanged)
+    }
+}
--- Elements Collection/ViewController.swift
+++ Elements Collection/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var customSegment: CustomSegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hello", view.bounds.width)
+        addSegment()
     }
 
+
+    func addSegment() {
+        customSegment.bgColor = UIColor(red: 0.94, green: 0.94, blue: 0.94, alpha: 1.00)
+        customSegment.buttonTitles = ["Segment 1", "Segment 2"]
+        customSegment.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+    }
+    
+    @objc func segmentedControlValueChanged(_ sender: CustomSegmentedControl) {
+        let selectedSegmentIndex = sender.selectedSegmentIndex
+        print("Selected segment index: \(selectedSegmentIndex)")
+        // Handle segmented control value change here
+    }
 
 }
 
